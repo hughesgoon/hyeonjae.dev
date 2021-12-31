7fe469dbf7c91f9de29cf717394cbcf6e7c3d5ce
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2021-12-31 12:03:01 +0000

    chore: Adapt blog meta data for mine
    Modify some meta data's for mine

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Hyeonjae.dev`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `Hughes(hyeonjae.kim)`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `hughesgoon`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `guswo0527`, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/favicon.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
     giscus: {
       repo: 'hughesgoon/hyeonjae.dev',
       repo_id: 'R_kgDOGbwwUQ',
@@ -28,7 +26,7 @@ module.exports = {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,9 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
-      Gatsby-starter-bee
+    ©<a href="https://github.com/hughesgoon">Hughes</a>, Built with{' '}
+    <a href="https://github.com/hughesgoon/hyeonjae.dev">
+      Gatsby
     </a>
   </footer>
 )
